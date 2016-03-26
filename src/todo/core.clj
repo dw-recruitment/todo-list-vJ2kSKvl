6cef28123f92db4bc0b885060ff104bfcc0ec0bc
Anonymous Candidate <anon@example.com>
2016-03-26 16:16:45 -0400

    Complete Step 2

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,6 +1,12 @@
-(ns todo.core)
+(ns todo.core
+  (:require
+    [ring.adapter.jetty :as ring]
+    [clojure.pprint]
+    [compojure.core :refer [defroutes GET]]
+    )
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes routes
+  (GET "/" [] "<h1>Under Construction!</h1> <br /> <img src='http://i.giphy.com/GsaGeoyPw24ww.gif' />"))
+
+(defonce server (ring/run-jetty #'routes {:port 3000 :join? false}))
