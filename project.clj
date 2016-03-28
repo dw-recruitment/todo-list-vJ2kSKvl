b57045529bc862b1223d73899dc808493240b6ad
Anonymous Candidate <anon@example.com>
2016-03-28 17:04:42 -0400

    Complete Step 4

--- project.clj
+++ project.clj
@@ -3,11 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [
+                  [org.clojure/clojure "1.8.0"]
                   [ring/ring-core "1.4.0"]
                   [ring/ring-jetty-adapter "1.4.0"]
                   [compojure "1.5.0"]
                   [environ "1.0.2"]
+                  [org.clojure/java.jdbc "0.5.0"]
+                  [org.postgresql/postgresql "9.4.1208.jre7"]
+                  [java-jdbc/dsl "0.1.3"]
                   ]
   :plugins [[lein-environ "1.0.2"]]
   :profiles {
--- src/todo/core.clj
+++ src/todo/core.clj
@@ -4,9 +4,34 @@
     [clojure.pprint]
     [environ.core :as environ]
     [compojure.core :refer [defroutes GET]]
-
+    [clojure.java.jdbc :as postgres]
+    [java-jdbc.ddl :as ddl]
     )
   )
+;
+
+(def db  {
+          :classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :subname "//127.0.0.1:5432/todo"
+          :user "dev"
+          :password "dev"}
+  )
+
+(postgres/db-do-commands db 
+  (ddl/create-table :ToDoList
+    [:task "varchar(50)"]
+    [:todo "boolean"]
+    [:done "boolean"]
+    )
+  )
+
+(postgres/insert! db :ToDoList :transaction? false 
+  {:task "Buy cat food." :todo true :done false}
+  {:task "Take out the trash." :todo true :done false})
+
+(println (postgres/query db 
+  ["SELECT * FROM todolist WHERE todo = ?" true]))
 
 (defroutes routes
   (GET "/" [] "<h1>Under Construction!</h1> <img src='http://i.giphy.com/GsaGeoyPw24ww.gif' />")
